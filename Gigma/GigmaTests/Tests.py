c55ebe7999fcf29d53b38a7ce71695a7c9992e2a
Jake <jpc21@ic.ac.uk>
2023-06-02 13:14:26 +0100

    jpc21: add python script to test functionality

---
+++ Gigma/GigmaTests/Tests.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+PASS = True
+FAIL = False
+
+class Tests:
+
+    exitCode = 0
+
+    def _runAll(self):
+        publicMethods = [method for method in dir(self) if callable(getattr(self, method)) if not method.startswith('_')]
+        for method in publicMethods:
+            if getattr(self, method)() == FAIL:
+                print(f"Failed test {method}")
+                self.exitCode += 1 
+            else:
+                print(f"Test passed")
+
+    def checkAppDelegateHeaderExists(self): 
+        PATH = "../Gigma/AppDelegate.h"
+        return os.path.exists(PATH)
+        
+
+run = Tests()
+run._runAll()
+sys.exit(run.exitCode)
