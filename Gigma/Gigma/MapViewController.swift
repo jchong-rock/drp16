8a249db513a118adfb421dd20c0df91ca18cea52
Jake <jpc21@ic.ac.uk>
2023-06-02 22:01:15 +0100

    jpc21: update MapViewController to use PostgreSQL driver and handle connection failures

--- Gigma/Gigma/MapViewController.swift
+++ Gigma/Gigma/MapViewController.swift
@@ -14,28 +14,36 @@ import NotificationCenter
 class MapViewController : UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
-    var data: DataBaseDriver = FestivalData.init()
+    var data: DataBaseDriver = PostgreSQLDriver()
     var mapCache: MapCache?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         mapView.delegate = self
         let festivalName = UserDefaults.standard.string(forKey: "FestivalIsSet")
         let config = MapCacheConfig(withUrlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
         mapCache = MapCache(withConfig: config)
         mapView.showsUserLocation = true;
+
+        let errorLocation = CLLocationCoordinate2DMake(51.5124801, -0.2182141)
+        let errorRegion = MKCoordinateRegion(center: mapView.userLocation.location?.coordinate ?? errorLocation, latitudinalMeters: 200, longitudinalMeters: 200)
+        mapView.setRegion(errorRegion, animated: true)
+        
         if (festivalName != "Unknown Festival") {
-            let festival = data.getFestival(name: festivalName! as String)
-            let centre = festival.centre.toCLCoordinate()
-            let width = festival.width
-            let height = festival.height
-            let coords = MKCoordinateRegion(center: centre, latitudinalMeters: width, longitudinalMeters: height)
-            mapView.setRegion(coords, animated: true)
-        } else {
-            let errorLocation = CLLocationCoordinate2DMake(51.5124801, -0.2182141)
-            let errorRegion = MKCoordinateRegion(center: mapView.userLocation.location?.coordinate ?? errorLocation, latitudinalMeters: 200, longitudinalMeters: 200)
-            mapView.setRegion(errorRegion, animated: true)
-            
+            let connectionSuccess = data.connect()
+            if connectionSuccess {
+                let festival = data.getFestival(name: festivalName! as String)
+                let centre = festival.centre.toCLCoordinate()
+                let width = festival.width
+                let height = festival.height
+                let coords = MKCoordinateRegion(center: centre, latitudinalMeters: width, longitudinalMeters: height)
+                mapView.setRegion(coords, animated: true)
+            } else {
+                let alert = UIAlertController(title: "Error", message: "Connection to database failed.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in }))
+                self.present(alert, animated: true, completion: nil)
+            }
         }
         mapView.useCache(mapCache!)
         let nc = NotificationCenter.default
