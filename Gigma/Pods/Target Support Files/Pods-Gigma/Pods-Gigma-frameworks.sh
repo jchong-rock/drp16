2a34be99bc2c757b21446bb6bb749d9c59de2578
Jake <jpc21@ic.ac.uk>
2023-06-06 15:49:49 +0100

    jpc21: add nRFMeshProvision pod

--- Gigma/Pods/Target Support Files/Pods-Gigma/Pods-Gigma-frameworks.sh
+++ Gigma/Pods/Target Support Files/Pods-Gigma/Pods-Gigma-frameworks.sh
@@ -176,10 +176,12 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CryptoSwift/CryptoSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MapCache/MapCache.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NSDate_Utils/NSDate_Utils.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CryptoSwift/CryptoSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MapCache/MapCache.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NSDate_Utils/NSDate_Utils.framework"
 fi
