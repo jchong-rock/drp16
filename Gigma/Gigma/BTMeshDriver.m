9b2dc269f42bb0a64c579d8c8922e162213a9b06
jpc21 <jpc21@perry04.doc.ic.ac.uk>
2023-06-07 20:01:57 +0100

    jpc21: add functionality to search for bluetooth devices

--- Gigma/Gigma/BTMeshDriver.m
+++ Gigma/Gigma/BTMeshDriver.m
@@ -9,6 +9,7 @@
 
 @interface BTMeshDriver () {
     NSMutableDictionary * currentlyAvailableDevices;
+    CBPeripheral * discoveredPeripheral;
     NSArray * kServices;
 }
 
@@ -51,7 +52,7 @@
         case CBManagerStatePoweredOn:
             stateString = @"Bluetooth is currently powered on and available to use.";
             NSLog(@"on");
-            [self.centralManager scanForPeripheralsWithServices: kServices options: nil];
+            //[self.centralManager scanForPeripheralsWithServices: kServices options: nil];
             break;
         default:
             stateString = @"State unknown, update imminent.";
@@ -66,9 +67,28 @@
     NSLog(@"%@", peripheral.identifier);
 }
 
+- (void) retrievePeripheral {
+    NSArray * connectedPeripherals = [centralManager retrieveConnectedPeripheralsWithServices: kServices];
+    NSLog(@"Found connected peripherals with transfer service: %@", connectedPeripherals);
+    
+    CBPeripheral * lastPeripheral = connectedPeripherals.lastObject;
+    
+    if (lastPeripheral != nil) {
+        NSLog(@"Connecting to peripheral: %@", lastPeripheral);
+        discoveredPeripheral = lastPeripheral;
+        [centralManager connectPeripheral: lastPeripheral options: nil];
+    } else {
+        [centralManager scanForPeripheralsWithServices: kServices options: @{
+            CBCentralManagerScanOptionAllowDuplicatesKey: @YES
+        }];
+    }
+}
+
 - (void) setupBluetooth {
     if (!self.centralManager) {
-        self.centralManager = [[CBCentralManager alloc] initWithDelegate: self queue: dispatch_get_main_queue()];
+        self.centralManager = [[CBCentralManager alloc] initWithDelegate: self queue: dispatch_get_main_queue() options: @{
+            CBCentralManagerOptionShowPowerAlertKey : @YES
+        }];
     }
     if (!self.peripheralManager) {
         self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate: self queue: dispatch_get_main_queue()];
@@ -89,7 +109,6 @@
 - (NSDictionary <NSUUID *, NSString *> * _Nonnull) nearbyBluetoothDevices {
     return currentlyAvailableDevices;
 }
-
 
 - (void) peripheralManagerDidUpdateState:(nonnull CBPeripheralManager *) peripheral {
     if (peripheral.state == CBManagerStatePoweredOn) {
