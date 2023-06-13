c13da80afbcec9f9df453963141ab4d4832a9c56
Jake <jpc21@ic.ac.uk>
2023-06-13 01:14:36 +0100

    jpc21: update chilkat

---
+++ Gigma/Gigma/MultipeerDriver.h
@@ -0,0 +1,12 @@
+//
+//  MultipeerDriver.h
+//  Gigma
+//
+//  Created by Jake Chong on 12/06/2023.
+//
+
+#ifndef MultipeerDriver_h
+#define MultipeerDriver_h
+
+
+#endif /* MultipeerDriver_h */
---
+++ Gigma/Gigma/MultipeerDriver.m
@@ -0,0 +1,8 @@
+//
+//  MultipeerDriver.m
+//  Gigma
+//
+//  Created by Jake Chong on 12/06/2023.
+//
+
+#import <Foundation/Foundation.h>
