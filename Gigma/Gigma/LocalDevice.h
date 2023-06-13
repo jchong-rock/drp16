6f3d2d3c8d26f6c4404829cca5b1b450160a5cb8
Jake <jpc21@ic.ac.uk>
2023-06-13 14:19:26 +0100

    jpc21: update chilkat

---
+++ Gigma/Gigma/LocalDevice.h
@@ -0,0 +1,12 @@
+//
+//  LocalDevice.h
+//  Gigma
+//
+//  Created by Jake Chong on 13/06/2023.
+//
+
+#ifndef LocalDevice_h
+#define LocalDevice_h
+
+
+#endif /* LocalDevice_h */
---
+++ Gigma/Gigma/LocalDevice.m
@@ -0,0 +1,8 @@
+//
+//  LocalDevice.m
+//  Gigma
+//
+//  Created by Jake Chong on 13/06/2023.
+//
+
+#import <Foundation/Foundation.h>
